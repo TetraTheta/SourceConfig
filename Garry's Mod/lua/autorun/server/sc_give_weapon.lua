d334a295474716e5258fda552f77b6cb1a5b2ceb
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-08-18 17:35:20 +0900

    [GMod] Update 'give weapon' script

--- Garry's Mod/lua/autorun/server/sc_give_weapon.lua
+++ Garry's Mod/lua/autorun/server/sc_give_weapon.lua
@@ -1,31 +1,37 @@
 --[[
-Give maximum ammo to player
+Give basic weapons to player
 
 Commands:
- - gca: Give maximum ammo to player
+ - gbw: Give basic weapons to player
 
 Requirements:
  - Player must be in 'superadmin' group
 --]]
 local adminPistolClass = "sc_adminpistol"
+local adminRifleClass = "sc_adminrifle"
 
 -- Give Admin Pistol to player
 local function GiveAdminGunWeapon(ply)
 	if not ply:IsValid() or not ply:IsUserGroup("superadmin") then return -1 end
 	ply:Give(adminPistolClass)
+	ply:Give(adminRifleClass)
 	ply:SendLua("RunConsoleCommand(\"use\", \"sc_adminpistol\")")
-	MsgN("AdminPistol is given to " .. ply:Nick())
+	MsgN("Admin Weapons are given to " .. ply:Nick())
 end
 
--- Give Admin Pistol, Gravity Gun, Physics Gun to player with command
+-- Give Admin Pistol, Admin Rifle, Gravity Gun, Physics Gun to player with command
 local function GiveBasicWeapons(ply, cmd, args, str)
 	if not ply:IsValid() or not ply:IsUserGroup("superadmin") then return -1 end
 	ply:Give(adminPistolClass)
+	ply:Give(adminRifleClass)
 	ply:Give("weapon_physgun")
 	ply:Give("weapon_physcannon")
+	if engine.ActiveGamemode() == "sandbox" then
+		ply:Give("gmod_tool")
+	end
 	ply:SendLua("RunConsoleCommand(\"use\", \"sc_adminpistol\")")
 	MsgN("Basic weapons are given to " .. ply:Nick())
 end
 
-concommand.Add("sc_gbw", GiveBasicWeapons, nil, "Give Basic weapon to player", 0)
+concommand.Add("gbw", GiveBasicWeapons, nil, "Give Basic weapon to player", 0)
 hook.Add("PlayerSpawn", "GiveAdminGunWeapon", GiveAdminGunWeapon)
