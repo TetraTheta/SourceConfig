6e9355f7355c56987cea0ad46dd5997ebf7db398
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-11-21 11:09:17 +0900

    Separate Garry's Mod scripts

--- Garry's Mod/lua/autorun/client/sc_enable_svcmd_client.lua
+++
@@ -1,4 +0,0 @@
-net.Receive("SVCMD", function(len, ply)
-	local message = net.ReadString()
-	chat.AddText(Color(255, 255, 255, 255), ply, message)
-end)
--- Garry's Mod/lua/autorun/client/sc_gca_client.lua
+++
@@ -1,5 +0,0 @@
-net.Receive("GCA", function(len, ply)
-	if net.ReadBool() then
-		surface.PlaySound("/items/ammo_pickup.wav")
-	end
-end)
