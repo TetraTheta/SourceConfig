27f948e1a5165a0a8b0ca7547c8bd8925cfe5f20
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2024-12-16 20:18:31 +0900

    [HL2] Add custom crosshair and resource
    These changes are done in 'halflife2.ttf' file, and it must be contained in VPK file to be work

--- HL2/copy.cmd
+++ HL2/copy.cmd
@@ -3,6 +3,6 @@
 set "source=%~dp0"
 set "dest=E:\Program Files\Steam\steamapps\common\Half-Life 2"
 
-xcopy "%source%" "%dest%" /B /E /Y
+xcopy "%source%" "%dest%" /B /E /Y /EXCLUDE:%~dp0\xcopy_exclude.txt
 
 pause
